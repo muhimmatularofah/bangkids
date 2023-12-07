52afbe5703511d6989e51aac48564c944de022e8
andhikap12 <marbunandhika@gmail.com>
2023-12-07 20:50:35 +0700

    ini udah kupush

---
+++ Capstone/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123Keren',
+  database: 'sellsmartly'
+})
+app.get('/kategori', (req, res) => {
+  connection.connect()
+
+connection.query('SELECT * from kategori', (err, rows, fields) => {
+  if (err) throw err
+  res.send(rows)
+})
+
+connection.end()
+})
+app.get('/produk', (req, res) => {
+  connection.connect()
+
+connection.query('SELECT * from produk', (err, rows, fields) => {
+  if (err) throw err
+  res.send(rows)
+})
+
+connection.end()
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
